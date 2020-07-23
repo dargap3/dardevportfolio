bcaf1cc36039838f320a510d77eeb8efb9bd2663
dargap3 <dargap@hotmail.com>
2020-07-23 02:21:48 -0500

    adding analitycs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import About from './components/About/about.component';
 import Projects from './components/Projects/projects.component';
 import Footer from './components/Footer/footer.component';
 
+import ReactGA from 'react-ga';
+
 import './App.css';
+
+
+const trackingId = "UA-173398640-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
 
 function App() {
   return (
