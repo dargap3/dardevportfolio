064ca698faff8e98d398b5b862dfcfb575ce5df6
dargap3 <dargap@hotmail.com>
2020-08-10 22:15:27 -0500

    Updates

---
+++ precache-manifest.33e36fbddceb9881b9935000029aa090.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a81874196e046c1161f2f1d3e9d14b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fc0a9dc024f3daf72f",
+    "url": "/static/css/main.9b3829ff.chunk.css"
+  },
+  {
+    "revision": "be4ab28cfb07a3e33b0e",
+    "url": "/static/js/2.842be8c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.842be8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2fc0a9dc024f3daf72f",
+    "url": "/static/js/main.988c5206.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "42fc19f350147d53d6f330b336a521f8",
+    "url": "/static/media/Dar.Dev-logo.42fc19f3.png"
+  },
+  {
+    "revision": "d28506960f76a5259b9b45cc7eabdf94",
+    "url": "/static/media/WhatsApp Image 2020-06-23 at 11.16.51 PM.d2850696.jpeg"
+  },
+  {
+    "revision": "cb7cb4310d7f9a6b1eca843b57994995",
+    "url": "/static/media/codepen.cb7cb431.svg"
+  },
+  {
+    "revision": "16b4073ce9f66f0f1410d783c7d22736",
+    "url": "/static/media/github.16b4073c.svg"
+  },
+  {
+    "revision": "49afdd269a87919a8bec90940e149cf0",
+    "url": "/static/media/linkedin.49afdd26.svg"
+  },
+  {
+    "revision": "885e64c7ea83630c062ada1066de1edd",
+    "url": "/static/media/twitter.885e64c7.svg"
+  }
+]);
